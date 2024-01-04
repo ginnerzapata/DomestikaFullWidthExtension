a6f15f9284b74a6e4833cb01b4208d252088040b
Ginner Zapata <gzapata@vizzn.ca>
2024-01-04 08:56:54 -0300

    fixed full width and hide menu

--- extension/content.js
+++ extension/content.js
@@ -3,11 +3,22 @@ let fullWidth = false;
 
 function adjustMaxWidth() {
   const container = document.querySelector(".course-page > .container");
+  const videoCol = document.querySelector(
+    ".course-page > .container > .row > .col-md-9"
+  );
+  const menuCol = document.querySelector(
+    ".course-page > .container > .row > .col-md-3"
+  );
+
   if (fullWidth) {
     container.classList.add("domestika-full-width");
+    videoCol.classList.add("col-md-12");
+    menuCol.classList.add("hide-menu");
     fullWidth = false;
   } else {
     container.classList.remove("domestika-full-width");
+    videoCol.classList.remove("col-md-12");
+    menuCol.classList.remove("hide-menu");
     fullWidth = true;
   }
 }
